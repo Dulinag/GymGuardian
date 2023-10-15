07046c6c436cc19a282d0aa7efa0a331f8f7da5a
Bennett Kautz <92739107+bkautz0824@users.noreply.github.com>
2023-10-15 17:10:09 -0400

    small changes

---
+++ Frontend/src/app/builder/set-builder/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+
+function SetBuilder({}) {
+  return (
+    <div>SetBuilder</div>
+  )
+}
+
+export default SetBuilder
